26712594f9cc084e2c5e2c28622866e6b2513985
Arshad khan <arshadkh507@gmail.com>
2023-08-23 16:57:38 +0500

    made own folder for python tutorial to save space

---
+++ 8.6. Conditional Execution.py
@@ -0,0 +1,29 @@
+
+# Write code to assign the string "You can apply to SI!" to output if the string "SI 106" is in the list courses. If it is not in courses, assign the value "Take SI 106!" to the variable output.
+""" 
+courses = ["ENGR 101", "SI 110", "ENG 125", "SI 106", "CHEM 130"]
+output = ""
+if "SI 106" in courses:
+    output = "You can apply to SI!"
+else:
+    output = "Take SI 106!"
+    
+print(output)
+"""
+
+# =======================================================
+# Create a variable, b, and assign it the value of 15. Then, write code to see if the value b is greater than that of a. If it is, a’s value should be multiplied by 2. If the value of b is less than or equal to a, nothing should happen. Finally, create variable c and assign it the value of the sum of a and b.
+
+"""
+a = 20
+b = 15
+if b > a:
+    a = a * 2
+else: 
+    c = a + b
+
+print(c)
+"""
+
+
+# =======================================================
